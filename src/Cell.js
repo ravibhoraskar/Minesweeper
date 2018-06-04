08db08e65134cd93867064aea98637abc08a11c0
Ravi Bhoraskar <rav@fb.com>
2018-06-04 15:42:20 -0700

    Fix lint warnings

--- src/Cell.js
+++ src/Cell.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-const MINE_CHAR = String.fromCodePoint(0x1f4a3);
-const WON_MINE_CHAR = String.fromCodePoint(0x1f3c6);
-const UNOPENED_SQUARE_CHAR = String.fromCodePoint(0x2b1b);
+const MINE_CHAR = String.fromCodePoint(0x1f4a3); // BOMB
+const WON_MINE_CHAR = String.fromCodePoint(0x1f3c6); // TROPHY
+const UNOPENED_SQUARE_CHAR = String.fromCodePoint(0x2b1b); // BLACK BOX
 
 class Cell extends Component {
 	render() {
@@ -20,7 +19,7 @@ class Cell extends Component {
 	}
 
 	_renderLost() {
-		if (this.props.value == MINE_CHAR) {
+		if (this.props.value === MINE_CHAR) {
 			return MINE_CHAR;
 		} else if (this.props.status) {
 			return this.props.value;
@@ -30,7 +29,7 @@ class Cell extends Component {
 	}
 
 	_renderWon() {
-		if (this.props.value == MINE_CHAR) {
+		if (this.props.value === MINE_CHAR) {
 			return WON_MINE_CHAR;
 		} else {
 			return this.props.value;
