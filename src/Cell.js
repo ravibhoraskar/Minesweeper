9f00f096c2194faed0759bcc50a7c1c274b9e0ce
Ravi Bhoraskar <rav@fb.com>
2018-06-06 23:16:18 -0700

    [refactor] Cell.js to use stateless functional component instead

--- src/Cell.js
+++ src/Cell.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 const MINE_CHAR = String.fromCodePoint(0x1f4a3); // BOMB
 const WON_MINE_CHAR = String.fromCodePoint(0x1f3c6); // TROPHY
 const UNOPENED_SQUARE_CHAR = String.fromCodePoint(0x2b1b); // BLACK BOX
 
-class Cell extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.youLost
-          ? this._renderLost()
-          : this.props.youWon
-            ? this._renderWon()
-            : this._renderPlaying()}
-      </div>
-    );
-  }
+const Cell = props => (
+  <div>
+    {props.youLost
+      ? _renderLost(props)
+      : props.youWon
+        ? _renderWon(props)
+        : _renderPlaying(props)}
+  </div>
+);
 
-  _renderLost() {
-    if (this.props.value === MINE_CHAR) {
-      return MINE_CHAR;
-    } else if (this.props.status) {
-      return this.props.value;
-    } else {
-      return UNOPENED_SQUARE_CHAR;
-    }
+const _renderLost = props => {
+  if (props.value === MINE_CHAR) {
+    return MINE_CHAR;
+  } else if (props.status) {
+    return props.value;
+  } else {
+    return UNOPENED_SQUARE_CHAR;
   }
+};
 
-  _renderWon() {
-    if (this.props.value === MINE_CHAR) {
-      return WON_MINE_CHAR;
-    } else {
-      return this.props.value;
-    }
+const _renderWon = props => {
+  if (props.value === MINE_CHAR) {
+    return WON_MINE_CHAR;
+  } else {
+    return props.value;
   }
+};
 
-  _renderPlaying() {
-    if (this.props.status) {
-      return this.props.value;
-    } else {
-      return UNOPENED_SQUARE_CHAR;
-    }
+const _renderPlaying = props => {
+  if (props.status) {
+    return props.value;
+  } else {
+    return UNOPENED_SQUARE_CHAR;
   }
-}
+};
+
 export default Cell;
